e69846792e158a753142d7c2bddb18282d21e99b
Benjamin Ellis <bsellis1@buffs.wtamu.edu>
2025-04-03 08:48:36 -0500

    Fixed Die.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Hello, World!");
 
 //Example use of Die class and roll method
 Die die = new Die();
-int rollResult = die.Roll();
+int rollResult = die.Roll(20);
 System.Console.WriteLine($"You rolled a dice value of {rollResult}");
 
 
