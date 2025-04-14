7eb5da06b5d51c85fe89a39ddcf3b70cb3ea20e9
Benjamin Ellis <bsellis1@buffs.wtamu.edu>
2025-04-14 13:21:24 -0500

    Commented out the tests

--- Program.cs
+++ Program.cs
@@ -4,42 +4,42 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("=== Manual Unit Test Runner ===\n");
+        // Console.WriteLine("=== Manual Unit Test Runner ===\n");
 
         // 1. Run message system tests (multi-language support)
-        MessagesTests msgTests = new MessagesTests();
-        msgTests.RunAllTests();
+        // MessagesTests msgTests = new MessagesTests();
+        // msgTests.RunAllTests();
 
         // 2. Run weapon unit tests
-        WeaponTest.RunWeaponUnitTests();
+        // WeaponTest.RunWeaponUnitTests();
 
         // 3. Run Die tests
-        DieTest dieTest = new DieTest();
-        dieTest.RunAllTests();
+        // DieTest dieTest = new DieTest();
+        // dieTest.RunAllTests();
 
         // 4. Run character unit tests
-        CharacterTest.RunPlayerUnitTest();
-        Console.WriteLine("=== End of Character Unit Tests ===\n");
+        // CharacterTest.RunPlayerUnitTest();
+        // Console.WriteLine("=== End of Character Unit Tests ===\n");
 
         // 5. Run dragon unit tests
-        DragonTests dragonTests = new DragonTests();
-        dragonTests.RunAllTests();
+        // DragonTests dragonTests = new DragonTests();
+        // dragonTests.RunAllTests();
 
         // 6. Run game startup test
-        GameTest gameTest = new GameTest();
-        gameTest.RunAllTests();
+        // GameTest gameTest = new GameTest();
+        // gameTest.RunAllTests();
 
         // 7. Run combat system test
-        CombatTests.RunAttackSequenceTest();
+        // CombatTests.RunAttackSequenceTest();
 
         // 8. Run branches unit tests
-        BranchesTests branchesTests = new BranchesTests();
-        branchesTests.RunAllTests();
+        // BranchesTests branchesTests = new BranchesTests();
+        // branchesTests.RunAllTests();
 
         // 9. Example use of Die class
-        Die die = new Die();
-        int rollResult = die.Roll(20);
-        Console.WriteLine($"You rolled a dice value of {rollResult}");
+        // Die die = new Die();
+        // int rollResult = die.Roll(20);
+        // Console.WriteLine($"You rolled a dice value of {rollResult}");
 
         // 10. Start the actual game
         Console.WriteLine("\n=== Game Start ===\n");
